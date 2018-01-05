dc76c9f051ad10ae801ac81bc162be31cbb986fb
eyarijk <eyarij@gmail.com>
2018-01-05 18:37:11 +0200

    Admin panel statistic

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Banner;
+use App\Category;
+use App\Contact;
+use App\Context;
+use App\Message;
+use App\Surfing;
+use App\Task;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -14,6 +21,25 @@ class ManageController extends Controller
     public function dashboard()
     {
         $user = User::find(auth()->id());
-        return view('admin.dashboard')->withUser($user);
+        $users = User::all()->count();
+        $balance = User::all()->sum('balance');
+        $categories = Category::all()->count();
+        $contact = Contact::where('is_show',true)->get()->count();
+        $tasks = Task::all()->count();
+        $surfing = Surfing::all()->count();
+        $message = Message::all()->count();
+        $context = Context::all()->count();
+        $banners = Banner::all()->count();
+        $all = $context + $banners;
+        return view('admin.dashboard')
+            ->withUser($user)
+            ->withUsers($users)
+            ->withBalance($balance)
+            ->withCategories($categories)
+            ->withContact($contact)
+            ->withTasks($tasks)
+            ->withSurfing($surfing)
+            ->withMessage($message)
+            ->withAll($all);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,4 +2,128 @@
 
 @section('content')
 
+    <div class="flex-container">
+        <div class="columns m-t-10">
+            <div class="column">
+                <h1 class="title">Краткая статистика</h1>
+            </div>
+        </div>
+        <hr class="m-t-0">
+
+        <div class="columns is-multiline">
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-user-circle m-r-5" aria-hidden="true"></i>Пользователей</h4>
+                                <p>
+                                   <span>Всего: <a href="{{route('users.index')}}">{{$users}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-credit-card-alt m-r-5" aria-hidden="true"></i>Всего денег</h4>
+                                <p>
+                                    <span>Всего: {{$balance}} ₽</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-bars m-r-5" aria-hidden="true"></i>Всего категорий</h4>
+                                <p>
+                                    <span>Всего: <a href="{{route('category.index')}}">{{$categories}} </a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-envelope-open m-r-5" aria-hidden="true"></i>Всего заявок</h4>
+                                <p>
+                                    <span>Всего: <a href="{{route('contact.admin')}}">{{$contact}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-handshake-o  m-r-5" aria-hidden="true"></i>Всего заданий</h4>
+                                <p>
+                                    <span>Всего: <a href="{{route('tasks.index')}}">{{$tasks}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-handshake-o  m-r-5" aria-hidden="true"></i>Всего серфинга</h4>
+                                <p>
+                                    <span>Всего: <a href="{{route('tasks.index')}}">{{$surfing}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-handshake-o  m-r-5" aria-hidden="true"></i>Всего писем</h4>
+                                <p>
+                                    <span>Всего: <a href="{{route('tasks.index')}}">{{$message}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div class="column is-one-quarter">
+                <div class="box">
+                    <article class="media">
+                        <div class="media-content">
+                            <div class="content">
+                                <h4 class="title"><i class="fa fa-handshake-o  m-r-5" aria-hidden="true"></i>Реклама</h4>
+                                <h6></h6>
+                                <p>
+                                    <span>Всего: <a href="{{route('tasks.index')}}">{{$all}}</a></span>
+                                </p>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
